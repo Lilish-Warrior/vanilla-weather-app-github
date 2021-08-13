1a3568a57b3884ed655f540b276db2bccc9d6497
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-08-13 16:57:30 +1000

    Created display weather function js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-let apiKey = "9e38b432e6a4b2912f3775e17cf35501";
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+}
+
+function searchCity(city) {
+  let apiKey = "9e38b432e6a4b2912f3775e17cf35501";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#button-addon2").value;
+  searchCity(city);
+}
