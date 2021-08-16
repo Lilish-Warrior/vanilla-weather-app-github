e16511c92ad7e2a7f009575d8056079b5ebb9cd5
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-08-16 13:59:47 +1000

    Improved h1 size

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
